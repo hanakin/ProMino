41581a9ca9c03aa88a469bd727ab7d158184208a
hanakin <midaym@gmail.com>
2013-07-02 17:33:46 +0900

    Add SASS & Compass to project
    Optimized some images
    Refreshed the ouputs
    minor tweaks

---
+++ config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :nested
+environment = :development
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+preferred_syntax = :sass
