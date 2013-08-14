e1c085fd1bf1a019774d58de0f9b622a4a0fa8e6
hanakin <midaym@gmail.com>
2013-08-14 17:10:59 +0900

    Add hover functionality for forumlist decripttions and recent posts as well as refactor forumlist css
    + Clean up forum list css
    + Add modified popover from BS3
    + Code functionality for handling popovers fr forumlist at all
    breakpoints

--- includes/forumlist.php
+++ includes/forumlist.php
@@ -4,28 +4,32 @@
 						<h2 class="forum-name h5"><a href="" title="FORUM">Forum</a></h2>
 						<ol class="forum-list list-unstyled">
 							<li class="forum-row row">
-								<hgroup class="col-sm-11">
-									<h2 class="forum-title h5 md-pull-left lg-pull-left"><a class="pull-left" href="">www.phpbb.com/community/</a></h2>
-									<dfn class="forum-description visible-sm">A link back to the support forum at www.phpbb.com.</dfn>
-									<span class="label pull-right hidden-sm">575</span>                            
-								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<hgroup class="col-sm-9">
+									<h2 class="forum-title h5"><a class="pull-left" href="">www.phpbb.com/community/</a></h2>
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">A link back to the support forum at www.phpbb.com.</p>
+									</dfn>
+								</hgroup>                          
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">575</span>  
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="text">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row row">
-								<hgroup class="col-sm-11">
-									<h2 class="forum-title h5 md-pull-left lg-pull-left"><a class="pull-left" href="">General Development Discussion</a></h2>
-									<dfn class="forum-description visible-sm">Discuss general development subjects that are not specific to a particular version like the versioning control system we use or other infrastructure.</dfn>
-									<span class="label pull-right hidden-sm">1289</span>                            
+								<hgroup class="col-sm-9">
+									<h2 class="forum-title h5"><a class="pull-left" href="">General Development Discussion</a></h2>
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">Discuss general development subjects that are not specific to a particular version like the versioning control system we use or other infrastructure.</p>
+									</dfn>                          
 								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">1289</span>  
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="Re: Git push account - for sh… ">Re: Git push account - for sh… <small>by <a href="" class="user" title="user">user</a> <time>May 07</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Git push account - … <small>by <a href="" class="user" title="user">user</a> <time>May 07</time></small></span>
 									</cite
 								</p>
 							</li>
@@ -35,107 +39,121 @@
 						<h2 class="forum-name h5"><a href="" title="PHPBB 3.x DEVELOPMENT">PHPBB 3.x Development</a></h2>
 						<ol class="forum-list list-unstyled">
 							<li class="forum-row row">
-								<hgroup class="col-sm-11">
-									<h2 class="forum-title h5 md-pull-left lg-pull-left"><a class="pull-left" href="">[3.x] Discussion</a></h2>
-									<dfn class="forum-description visible-sm">General discussion of development ideas and the approaches taken in the 3.x branch of phpBB. The next feature release of phpBB 3 will be 3.1/Ascreaus followed by 3.2/Arsia.</dfn>
-									<span class="label pull-right hidden-sm">4429</span>                            
+								<hgroup class="col-sm-9">
+									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] Discussion</a></h2>
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">General discussion of development ideas and the approaches taken in the 3.x branch of phpBB. The next feature release of phpBB 3 will be 3.1/Ascreaus followed by 3.2/Arsia.</p>
+									</dfn>                           
 								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">4429</span> 
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="text">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row row">
-								<hgroup class="col-sm-11">
+								<hgroup class="col-sm-9">
 									<span class="forum-icon glyphicon glyphicon-plus pull-left"></span>
-									<h2 class="forum-title h5 md-pull-left lg-pull-left"><a class="pull-left" href="">[3.x] RFCs</a></h2>
-									<dfn class="forum-description visible-sm">Publish your own request for comments or patches for the next version of phpBB. Discuss the contributions and proposals of others. Upcoming releases are 3.1/Ascraeus and 3.2/Arsia.</dfn>
-									<span class="label pull-right hidden-sm">4862</span>                            
+									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] RFCs</a></h2>
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">Publish your own request for comments or patches for the next version of phpBB. Discuss the contributions and proposals of others. Upcoming releases are 3.1/Ascraeus and 3.2/Arsia.</p>
+									</dfn>                          
 								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">4862</span>  
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="text">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row forum-sub row">
-								<hgroup class="col-sm-11">
-									<h2 class="forum-title h5 md-pull-left lg-pull-left"><a class="pull-left" href="">[3.x] Event Requests</a></h2>
-									<dfn class="forum-description visible-sm">Request hook events and what data needs to be sent for the new hook system.</dfn>
-									<span class="label pull-right hidden-sm">61</span>                            
+								<hgroup class="col-sm-9">
+									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] Event Requests</a></h2>
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">Request hook events and what data needs to be sent for the new hook system.</p>
+									</dfn>                          
 								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">61</span>  
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="text">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row row">
-								<hgroup class="col-sm-11">
+								<hgroup class="col-sm-9">
 									<span class="forum-icon glyphicon glyphicon-plus pull-left"></span>
-									<h2 class="forum-title h5 md-pull-left lg-pull-left">
+									<h2 class="forum-title h5">
 										<a class="pull-left" href="">[3.1/Ascraeus] Merged RFCs</a> 
 										<p class="forum-labels md-pull-left lg-pull-left visible-sm">
-											<span class="forum-label badge mine">0</span>
+											<!-- <span class="forum-label badge mine">0</span> -->
 											<span class="forum-label label locked unread">Locked</span>
 										</p>
 										<p class="forum-labels md-pull-left lg-pull-left hidden-sm">
-											<span class="forum-label badge mine">0</span>
+											<!-- <span class="forum-label badge mine">0</span> -->
 											<span class="forum-label label locked">Locked</span>
 											<span class="forum-label label unread">New</span>
 										</p>
 									</h2>
-									<dfn class="forum-description visible-sm">These requests for comments have lead to an implemented feature that has been successfully merged into the 3.1/Ascraeus branch. Everything listed in this forum will be available in phpBB 3.1.</dfn>
-									<span class="label pull-right hidden-sm">2821</span>                            
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">These requests for comments have lead to an implemented feature that has been successfully merged into the 3.1/Ascraeus branch. Everything listed in this forum will be available in phpBB 3.1.</p>
+									</dfn>                        
 								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">2821</span>    
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="text">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row forum-sub row">
-								<hgroup class="col-sm-11">
-									<h2 class="forum-title h5 md-pull-left lg-pull-left"><a class="pull-left" href="">[3.1/Ascraeus] Merged Event Requests</a></h2>
-									<dfn class="forum-description visible-sm">These requests for events in core phpBB have been merged into 3.1/Ascraeus and will be available with the next release.</dfn>
-									<span class="label pull-right hidden-sm">79</span>                            
+								<hgroup class="col-sm-9">
+									<h2 class="forum-title h5"><a class="pull-left" href="">[3.1/Ascraeus] Merged Event Requests</a></h2>
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">These requests for events in core phpBB have been merged into 3.1/Ascraeus and will be available with the next release.</p>
+									</dfn>                          
 								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">79</span>  
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="text">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row row">
-								<hgroup class="col-sm-11">
+								<hgroup class="col-sm-9">
 									<span class="forum-icon glyphicon glyphicon-plus pull-left"></span>
-									<h2 class="forum-title h5 md-pull-left lg-pull-left"><a class="pull-left" href="">[3.x] Rejected RFCs</a></h2>
-									<dfn class="forum-description visible-sm">These RFCs were either rejected or have been replaced by an alternative proposal. They will not be included in phpBB.</dfn>
-									<span class="label pull-right hidden-sm">841</span>                            
+									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] Rejected RFCs</a></h2>
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">These RFCs were either rejected or have been replaced by an alternative proposal. They will not be included in phpBB.</p>
+									</dfn>                           
 								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">841</span> 
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="text">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row forum-sub row">
-								<hgroup class="col-sm-11">
-									<h2 class="forum-title h5 md-pull-left lg-pull-left"><a class="pull-left" href="">[3.x] Rejected Event Requests</a></h2>
-									<dfn class="forum-description visible-sm">These event requests have either been rejected by the community or have become redundant because of code changes or other events requests.</dfn>
-									<span class="label pull-right hidden-sm">40</span>                            
+								<hgroup class="col-sm-9">
+									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] Rejected Event Requests</a></h2>
+									<dfn class="forum-description popover bottom">
+										<b class="arrow"></b><p class="popover-content">These event requests have either been rejected by the community or have become redundant because of code changes or other events requests.</p>
+									</dfn>                         
 								</hgroup>
-								<p class="forum-recent col-sm-1 hidden-sm">
+								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
+									<span class="label pull-left hidden-sm">40</span>   
 									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
-									<cite class="hide">
-										<a href="" title="text">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></a>
+									<cite class="popover left">
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
