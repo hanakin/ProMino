c0a9df00655f76d5ba32b8e63e229b4ff89d89a7
hanakin <midaym@gmail.com>
2013-08-14 22:21:42 +0900

    Info Section Semantic fixes & Label-list fixes
    * Changed renderering of Legend to be list for better semantics
    * Changed Birthday list to be a list of lists for better semantics and
    spacing as well as removing irelivent commas
    + Added # to all hrefs for consistancey
    + Added user titles for consistency and to test colors for user ranks
    + Added support for user rank colors on label-lists
    + Added one of each user to birthday list for testing
    + Added one of each user to online list for testing

--- includes/info.php
+++ includes/info.php
@@ -10,82 +10,115 @@
 					<dd><span class="label">19245</span></dd>
 
 					<dt>Newest Member</dt>
-					<dd><a href="" title="USER: kalasweet"><span class="label user">kalasweet</span></a></dd>
+					<dd><a href="#" title="USER: kalasweet"><span class="label user">kalasweet</span></a></dd>
 				</dl>
 				<article class="info-online panel hidden-sm">
   					<div class="info-online-legend panel-heading">
-  						<a href="" title="Administrators"><span class="label admint">Administrators</span></a> 
-		  				<a href="" title="Developers"><span class="label dev">Developers</span></a>
-		  				<a href="" title="Moderators"><span class="label modt">Moderators</span></a> 
-		  				<a href="" title="Styles Team"><span class="label stylet">Styles Team</span></a> 
-		  				<a href="" title="Support Team"><span class="label supt">Support Team</span></a> 
-		  				<a href="" title="Website Team"><span class="label webt">Website Team</span></a>
-		  				<a href="" title="Registered User"><span class="label user">Registered User</span></a>
-  						
+  						<ul class="list-inline">
+  							<li><a href="#" title="Administrators"><span class="label admint">Administrators</span></a></li>
+		  					<li><a href="#" title="Developers"><span class="label dev">Developers</span></a></li>
+		  					<li><a href="#" title="Moderators"><span class="label modt">Moderators</span></a></li>
+		  					<li><a href="#" title="Styles Team"><span class="label stylet">Styles Team</span></a></li>
+		  					<li><a href="#" title="Support Team"><span class="label supt">Support Team</span></a> </li>
+		  					<li><a href="#" title="Website Team"><span class="label webt">Website Team</span></a></li>
+		  					<li><a href="#" title="Registered User"><span class="label user">Registered User</span></a></li>
+  						</ul>
   					</div>
   					<dl class="info-online-list dl-horizontal">
 						<dt>Users Online: </dt>
 						<dd class="label-name bot">
-							<a href="" title="" class="bot">Bing [Bot]</a>, 
-							<a href="" title="" class="bot">Exabot [Bot]</a>, 
-							<a href="" title="" class="bot">Google [Bot]</a>, 
-							<a href="" title="" class="bot">Google Feedfetcher</a>, 
-							<a href="" title="" class="user">hanakin</a>, 
-							<a href="" title="" class="bot">Majestic-12 [Bot]</a>, 
-							<a href="" title="" class="supt">Noxwizard</a>, 
-							<a href="" title="" class="bot">Yahoo Slurp</a></dd>
+							<a href="#" title="BOT: Bing" class="bot">Bing [Bot]</a>, 
+							<a href="#" class="stylet" title="STYLES: flo87">flo87</a>, 
+							<a href="#" title="BOT: Exabot" class="bot">Exabot [Bot]</a>, 
+							<a href="#" class="dev" title="DEV: Pyramide">Pyramide</a>, 
+							<a href="#" title="BOT: Google" class="bot">Google [Bot]</a>, 
+							<a href="#" class="admint" title="ADMIN: MirabellAC">MirabellAC</a>, 
+							<a href="#" title="BOT: Google Feedfetcher" class="bot">Google Feedfetcher</a>, 
+							<a href="#" title="USER: hanakin" class="user">hanakin</a>, 
+							<a href="#" title="BOT: Majestic-12" class="bot">Majestic-12 [Bot]</a>, 
+							<a href="#" class="webt" title="WEB: sawo1955">sawo1955</a>, 
+							<a href="#" title="SUPT: Noxwizard" class="supt">Noxwizard</a>, 
+							<a href="#" title="BOT: Yahoo Slurp" class="bot">Yahoo Slurp</a>, 
+							<a href="#" class="modt" title="MOD: danieldevil">danieldevil</a>
+						</dd>
 					</dl>
 					<dl class="info-birthdays dl-horizontal">
 						<dt>Birthday Congrates to: </dt>
 						<dd>
-							<a href="" title="USER: Psyao"><span class="label user">Psyao</span></a>,
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">ErzengelAzrael</a></li>
-								<li class="label-list-right"><span>48</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">PartizanE</a></li>
-								<li class="label-list-right"><span>43</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">MirabellAC</a></li>
-								<li class="label-list-right"><span>38</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">sawo1955</a></li>
-								<li class="label-list-right"><span>35</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">EkspertRu</a></li>
-								<li class="label-list-right"><span>29</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">Pyramide</a></li>
-								<li class="label-list-right"><span>28</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">Tar-Minyatur</a></li>
-								<li class="label-list-right"><span>28</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">danieldevil</a></li>
-								<li class="label-list-right"><span>24</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">Bruno_me</a></li>
-								<li class="label-list-right"><span>24</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">flo87</a></li>
-								<li class="label-list-right"><span>23</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">NittoHelp</a></li>
-								<li class="label-list-right"><span>23</span></li>
-							</ul>, 
-							<ul class="label-list">
-								<li class="label-list-left"><a href="" title="">_LB</a></li>
-								<li class="label-list-right"><span>18</span></li>
+							<ul class="list-inline">
+								<li><a href="#" title="USER: Psyao"><span class="label user">Psyao</span></a>
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" title="USER: ErzengelAzrael">ErzengelAzrael</a></li>
+										<li class="label-list-right"><span>48</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" title="USER: PartizanE">PartizanE</a></li>
+										<li class="label-list-right"><span>43</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" class="admint" title="ADMIN: MirabellAC">MirabellAC</a></li>
+										<li class="label-list-right"><span>38</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" class="webt" title="WEB: sawo1955">sawo1955</a></li>
+										<li class="label-list-right"><span>35</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" title="USER: EkspertRu">EkspertRu</a></li>
+										<li class="label-list-right"><span>29</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" class="dev" title="DEV: Pyramide">Pyramide</a></li>
+										<li class="label-list-right"><span>28</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" title="USER: Tar-Minyatur">Tar-Minyatur</a></li>
+										<li class="label-list-right"><span>28</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" class="modt" title="MOD: danieldevil">danieldevil</a></li>
+										<li class="label-list-right"><span>24</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" title="USER: Bruno_me">Bruno_me</a></li>
+										<li class="label-list-right"><span>24</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" class="stylet" title="STYLES: flo87">flo87</a></li>
+										<li class="label-list-right"><span>23</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" title="USER: NittoHelp">NittoHelp</a></li>
+										<li class="label-list-right"><span>23</span></li>
+									</ul>
+								</li>	
+								<li>
+									<ul class="label-list">
+										<li class="label-list-left"><a href="#" class="supt" title="SUPT: _LB">_LB</a></li>
+										<li class="label-list-right"><span>18</span></li>
+									</ul>
+								</li>	
 							</ul>
 						</dd>
 					</dl>
