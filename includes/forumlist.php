2cb9dd3266e56e17f46e164d03214e5b60d06c12
hanakin <midaym@gmail.com>
2013-08-14 22:26:23 +0900

    Add # to remaining hrefs for consistancy

--- includes/forumlist.php
+++ includes/forumlist.php
@@ -1,86 +1,86 @@
 			<section class="forums">
 				<ol class="list-unstyled">
 					<li class="forum">
-						<h2 class="forum-name h5"><a href="" title="FORUM">Forum</a></h2>
+						<h2 class="forum-name h5"><a href="#" title="FORUM">Forum</a></h2>
 						<ol class="forum-list list-unstyled">
 							<li class="forum-row row">
 								<hgroup class="col-sm-9">
-									<h2 class="forum-title h5"><a class="pull-left" href="">www.phpbb.com/community/</a></h2>
+									<h2 class="forum-title h5"><a class="pull-left" href="#">www.phpbb.com/community/</a></h2>
 									<dfn class="forum-description popover bottom">
 										<b class="arrow"></b><p class="popover-content">A link back to the support forum at www.phpbb.com.</p>
 									</dfn>
 								</hgroup>                          
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">575</span>  
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Text <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Text <small>by <a href="#" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row row">
 								<hgroup class="col-sm-9">
-									<h2 class="forum-title h5"><a class="pull-left" href="">General Development Discussion</a></h2>
+									<h2 class="forum-title h5"><a class="pull-left" href="#">General Development Discussion</a></h2>
 									<dfn class="forum-description popover bottom">
 										<b class="arrow"></b><p class="popover-content">Discuss general development subjects that are not specific to a particular version like the versioning control system we use or other infrastructure.</p>
 									</dfn>                          
 								</hgroup>
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">1289</span>  
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Git push account - … <small>by <a href="" class="user" title="user">user</a> <time>May 07</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Git push account - … <small>by <a href="#" class="user" title="user">user</a> <time>May 07</time></small></span>
 									</cite
 								</p>
 							</li>
 						</ol>
 					</li>
 					<li class="forum">
-						<h2 class="forum-name h5"><a href="" title="PHPBB 3.x DEVELOPMENT">PHPBB 3.x Development</a></h2>
+						<h2 class="forum-name h5"><a href="#" title="PHPBB 3.x DEVELOPMENT">PHPBB 3.x Development</a></h2>
 						<ol class="forum-list list-unstyled">
 							<li class="forum-row row">
 								<hgroup class="col-sm-9">
-									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] Discussion</a></h2>
+									<h2 class="forum-title h5"><a class="pull-left" href="#">[3.x] Discussion</a></h2>
 									<dfn class="forum-description popover bottom">
 										<b class="arrow"></b><p class="popover-content">General discussion of development ideas and the approaches taken in the 3.x branch of phpBB. The next feature release of phpBB 3 will be 3.1/Ascreaus followed by 3.2/Arsia.</p>
 									</dfn>                           
 								</hgroup>
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">4429</span> 
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="#" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row row">
 								<hgroup class="col-sm-9">
 									<span class="forum-icon glyphicon glyphicon-plus pull-left"></span>
-									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] RFCs</a></h2>
+									<h2 class="forum-title h5"><a class="pull-left" href="#">[3.x] RFCs</a></h2>
 									<dfn class="forum-description popover bottom">
 										<b class="arrow"></b><p class="popover-content">Publish your own request for comments or patches for the next version of phpBB. Discuss the contributions and proposals of others. Upcoming releases are 3.1/Ascraeus and 3.2/Arsia.</p>
 									</dfn>                          
 								</hgroup>
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">4862</span>  
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="#" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row forum-sub row">
 								<hgroup class="col-sm-9">
-									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] Event Requests</a></h2>
+									<h2 class="forum-title h5"><a class="pull-left" href="#">[3.x] Event Requests</a></h2>
 									<dfn class="forum-description popover bottom">
 										<b class="arrow"></b><p class="popover-content">Request hook events and what data needs to be sent for the new hook system.</p>
 									</dfn>                          
 								</hgroup>
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">61</span>  
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="#" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
@@ -88,7 +88,7 @@
 								<hgroup class="col-sm-9">
 									<span class="forum-icon glyphicon glyphicon-plus pull-left"></span>
 									<h2 class="forum-title h5">
-										<a class="pull-left" href="">[3.1/Ascraeus] Merged RFCs</a> 
+										<a class="pull-left" href="#">[3.1/Ascraeus] Merged RFCs</a> 
 										<p class="forum-labels md-pull-left lg-pull-left visible-sm">
 											<!-- <span class="forum-label badge mine">0</span> -->
 											<span class="forum-label label locked unread">Locked</span>
@@ -105,55 +105,55 @@
 								</hgroup>
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">2821</span>    
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="#" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row forum-sub row">
 								<hgroup class="col-sm-9">
-									<h2 class="forum-title h5"><a class="pull-left" href="">[3.1/Ascraeus] Merged Event Requests</a></h2>
+									<h2 class="forum-title h5"><a class="pull-left" href="#">[3.1/Ascraeus] Merged Event Requests</a></h2>
 									<dfn class="forum-description popover bottom">
 										<b class="arrow"></b><p class="popover-content">These requests for events in core phpBB have been merged into 3.1/Ascraeus and will be available with the next release.</p>
 									</dfn>                          
 								</hgroup>
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">79</span>  
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="#" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row row">
 								<hgroup class="col-sm-9">
 									<span class="forum-icon glyphicon glyphicon-plus pull-left"></span>
-									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] Rejected RFCs</a></h2>
+									<h2 class="forum-title h5"><a class="pull-left" href="#">[3.x] Rejected RFCs</a></h2>
 									<dfn class="forum-description popover bottom">
 										<b class="arrow"></b><p class="popover-content">These RFCs were either rejected or have been replaced by an alternative proposal. They will not be included in phpBB.</p>
 									</dfn>                           
 								</hgroup>
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">841</span> 
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="#" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
 							<li class="forum-row forum-sub row">
 								<hgroup class="col-sm-9">
-									<h2 class="forum-title h5"><a class="pull-left" href="">[3.x] Rejected Event Requests</a></h2>
+									<h2 class="forum-title h5"><a class="pull-left" href="#">[3.x] Rejected Event Requests</a></h2>
 									<dfn class="forum-description popover bottom">
 										<b class="arrow"></b><p class="popover-content">These event requests have either been rejected by the community or have become redundant because of code changes or other events requests.</p>
 									</dfn>                         
 								</hgroup>
 								<p class="forum-recent col-sm-2 col-sm-offset-1 hidden-sm">
 									<span class="label pull-left hidden-sm">40</span>   
-									<a class="pull-right" href="" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
+									<a class="pull-right" href="#" title=""><span class="glyphicon glyphicon-chevron-right"></span></a>
 									<cite class="popover left">
-										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
+										<b class="arrow"></b><span class="popover-content">Text  <small>by <a href="#" class="user" title="user">user</a> <time>22 minutes ago</time></small></span>
 									</cite
 								</p>
 							</li>
--- includes/navbar.php
+++ includes/navbar.php
@@ -2,20 +2,20 @@
 				<nav class="header-mobile-nav visible-sm">
 					<ul class="list-inline row">
 						<li class="col-4">
-							<a class="pull-left" href="" title="Menu"><span class="glyphicon glyphicon-align-justify"></span>  <span class="badge">5</span></a>
+							<a class="pull-left" href="#" title="Menu"><span class="glyphicon glyphicon-align-justify"></span>  <span class="badge">5</span></a>
 						</li>
 						<li class="col-4">
-							<a class="center-block header-logo-mobile" href="#"><img src="img/logo.png" height="40px" alt="PHPBB" /></a>
+							<h1 class="center-block header-logo-mobile"><a href="#"><img src="img/logo.png" height="40px" alt="PHPBB | Creating Communities" /><span class="text-hide">PHPBB</span></a></h1>
 						</li>
 						<li class="col-4">
-							<a class="pull-right" href="" title="Moderator Control Panel"><span class="glyphicon glyphicon-cog"></span></a>
+							<a class="pull-right" href="#" title="Moderator Control Panel"><span class="glyphicon glyphicon-cog"></span></a>
 						</li>
 					</ul>
 				</nav>
 				<hgroup class="row hidden-sm">
-					<a class="header-logo col-lg-2 col-sm-3" href="#"><img src="img/logo.png" height="40px" alt="PHPBB" /></a>
+					<h1 class="header-logo col-lg-2 col-sm-3"><a href="#"><img src="img/logo.png" height="40px" alt="PHPBB | Creating Communities" /><span class="text-hide">PHPBB</span></a></h1>
 					<form action="" class="header-form form hidden-sm col-lg-3 col-sm-3">
-							<input type="text" class="search form-control input-sm" placeholder="Search... " />
+							<input type="text" class="search form-control input-sm" placeholder="Search... " name="Search" title="Search" />
 					</form>
 					<nav class="header-nav col-lg-7 col-sm-6">
 						<ul class="list-inline row">
@@ -26,11 +26,11 @@
 							</li>
 							<li class="pull-right">
 								<ul class="list-inline">
-									<li class="header-avatar pull-left"><a href="" title="Control Panel"><img src="img/user.jpg" width="30px" height="30px" alt="user" /> user</a></li>
-									<li><a href="" title="Notifications"><span class="glyphicon glyphicon-inbox"></span> <span class="text-hide">Notifications</span> <span class="badge">5</span></a></li>
-									<li><a href="" title="Private Messages"><span class="glyphicon glyphicon-envelope"></span> <span class="text-hide">PMs</span> <span class="badge">2</span> </a></li>
-									<li><a href="" title="Logout"><span class="glyphicon glyphicon-off"></span> <span class="text-hide">Logout</span> </a></li>
-									<li><a href="" title="Moderator Control Panel"><span class="glyphicon glyphicon-cog"></span> <span class="text-hide">Moderator Control Panel</span> </a></li>
+									<li class="header-avatar pull-left"><a href="#" title="Control Panel"><img src="img/user.jpg" width="30px" height="30px" alt="USER: user" /> user</a></li>
+									<li><a href="#" title="Notifications"><span class="glyphicon glyphicon-inbox"></span> <span class="text-hide">Notifications</span> <span class="badge">5</span></a></li>
+									<li><a href="#" title="Private Messages"><span class="glyphicon glyphicon-envelope"></span> <span class="text-hide">PMs</span> <span class="badge">2</span> </a></li>
+									<li><a href="#" title="Logout"><span class="glyphicon glyphicon-off"></span> <span class="text-hide">Logout</span> </a></li>
+									<li><a href="#" title="Moderator Control Panel"><span class="glyphicon glyphicon-cog"></span> <span class="text-hide">Moderator Control Panel</span> </a></li>
 								</ul>
 							</li>
 						</ul>
