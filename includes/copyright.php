d6509ca3c5ea4e7c99b6222b5fb73b2097b926f2
hanakin <midaym@gmail.com>
2013-08-14 22:24:15 +0900

    Make copyright a semantic nav list

--- includes/copyright.php
+++ includes/copyright.php
@@ -1,4 +1,10 @@
 			<footer class="footer text-center">
-				<p><a href="" title="">The team</a> • <a href="" title="">Delete all board cookies</a> • All times are GMT+00:00</p>
+				<ul class="list-inline">
+					<li><a href="#" title="">The team</a></li>
+					<li>&bull;</li>
+					<li><a href="#" title="">Delete all board cookies</a></li>
+					<li>&bull;</li>
+					<li>All times are GMT+00:00</li>
+				</ul>
 				<p>Powered by <a href="http://www.phpbb.com" title="">phpBB</a>® Forum Software © phpBB Group </p>
 			</footer>
