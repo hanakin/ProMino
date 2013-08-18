2b266cde3ef47467bfc172126e172964cf71bca5
hanakin <midaym@gmail.com>
2013-08-18 17:20:46 +0900

    hide toolbar on mobile

--- includes/toolbar.php
+++ includes/toolbar.php
@@ -1,4 +1,4 @@
-		<div class="toolbar row">
+		<div class="toolbar row hidden-sm">
 			<ul class="toolbar-tools label-list pull-right clearfix hidden-sm">
 				<li class="label-list-left"><span class="label">Wed Aug 14, 2013 11:27 am</span></li>
 				<li class="label-list-right"><a href="#" data-toogle="tooltip" data-placement="bottom" data-container=".toolbar" title="Mark Forums Read"><span class="label"><span class="glyphicon glyphicon-check"></span> <span class="text-hide">Mark All Forums Read</span></span></a></li>
